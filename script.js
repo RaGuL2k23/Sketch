2c9897b08bfaeff72fd08488005f8dc78c74c21f
ragul2k23 <rockyragulsiva2005@gmail.com>
2023-08-31 00:11:53 +0530

    All Logic Done

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
-let noOfDiv = 16;
- 
-const grid = document.createElement("div")
-grid.className= 'grid';
-for(let i = 0; i <noOfDiv ; i++){
-    const column = document.createElement("div");
-    column.className= 'column';
-4
-    for(let j=0;j < noOfDiv ; j++){
-        const row = document.createElement("div");
-        row.className = 'row';
-        column.appendChild(row);
-    }
-    grid.appendChild(column);
-}
-document.body.appendChild(grid);
+let exisistingGrid=null;
+let rows = [];
 let color='';
 function getColor(){
-      color = document.getElementById("input1").value
-        
-    }
-     
-const rows = document.querySelectorAll(".row");
-rows.forEach(rows => {
-   
-   rows.addEventListener('mouseover',(e)=> {
-   e.target.style.cssText=`background-color:${color}`})
-});
+    color = document.getElementById("input1").value
+      return color;
+  }
+function call(){
+    let n =prompt("enter no b/w 1 to 100");
+    grid(n);
+    
+}
+ const cont = document.createElement("div");
+ cont.className = 'container';
+ document.body.append(cont);
  
+function grid(noOfDiv){
+    const grid = document.createElement("div")
+    grid.className= 'grid';
+    for(let i = 0; i <noOfDiv ; i++){
+        let  column = document.createElement("div");
+        column.className= 'column';
+    
+        for(let j=0;j < noOfDiv ; j++){
+             let row = document.createElement("div");
+            row.className = 'row';
+            rows.push(row);
+            console.log(row);
+            column.appendChild(row);
+        }
+        grid.appendChild(column);
+    }   
+    if(exisistingGrid===null){
+        cont.appendChild(grid);
+        exisistingGrid = true;
+    }
+    else{
+       cont.removeChild(cont.firstChild);
+       cont.appendChild(grid);
+        exisistingGrid=null;
+    }
+    rows.forEach(rows => {
+       
+        rows.addEventListener('mouseover',(e)=> {
+        e.target.style.cssText=`background-color:${color}`})
+     });
+    
+}
+ 
+
+
+
+ 
+
+     console.log(rows)
+      
+   
